14ad2eb16849ba49b73fb91f533d71100c520649
Sugandha-Herath <sugandhaherath01@gmail.com>
2025-12-26 19:56:13 +0530

    update

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ function addTask(){
     if(inputBox.value===''){
         alert("You must write something!!!");
     }else{
+        // create new HTML element ,"li" means list item element [Same as writing <li> </li> in HTML ] 
+        // [let li]===> Stores the created <li> element in a variable called li
         let li=document.createElement("li");
+        
+        // Takes the text typed by the user in an input field and Puts that text inside the <li>
+        // inputBox.value = user-entered text
         li.innerHTML=inputBox.value;
+
+        //Adds the newly created <li> inside a list
         listContainer.appendChild(li);
 
         let span=document.createElement("span");
